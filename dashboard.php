26a151ae16a74d2004b8967405942d72901e65ab
Payal246 <payal@hastishah.com>
2021-09-02 17:27:15 +0530

    Comment removed

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@ error_reporting (E_ERROR);
 include "connection.php";
 
 ?>
-jjjjkkkjj
+
 <html>
 <head><title>Dashboard</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
